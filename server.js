778bddec81910cfca73eec651bdebda476b7edc1
clintonjnelson <clintonjnelson@live.com>
2015-06-13 19:23:04 -0700

    add basic server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express');
+var app     = express();
+
+// Routers
+var productRouter  = express.Router();
+
+// Load Routers
+require('./routes/product_router.js')(app);
+
+// Use Routes
+app.use(productRouter);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server running on port: ', process.env.PORT || 3000);
+});
+
+
+
